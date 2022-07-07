a4ce96e7794765cc4c95a381d571843600cbc12f
Reid McCulloch <wrm7914@stern.nyu.edu>
2022-07-07 19:55:20 -0400

    Added new python file

--- my_script.py
+++ my_script.py
@@ -1,4 +1,10 @@
-# my-first-repo# My First Repo!
+# this is the "my_script.py" file...
+
+print("HELLO WORLD!")
+
+x = 2 + 2
+print(x)
+# My First Repo!
 
 This is the README.md file. It uses the markdown language. The line above is a heading, starting with the # sign. Don't be confused that # means something different in .md and .py files.
 
@@ -10,9 +16,4 @@ And lists too:
   + Item 2
   + Item 3
 
-For more information about Markdown syntax, see the [Markdown Guide](https://www.markdownguide.org/basic-syntax/).# this is the "my_script.py" file...
-
-print("HELLO WORLD!")
-
-x = 2 + 2
-print(x)
+For more information about Markdown syntax, see the [Markdown Guide](https://www.markdownguide.org/basic-syntax/).
